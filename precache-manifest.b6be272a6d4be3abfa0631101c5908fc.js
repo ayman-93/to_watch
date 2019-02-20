dfb271ccde9aa847ae3cb206fa736e9f6be2714e
ayman-93 <aymanfaisal.cs@gmail.com>
2019-02-20 16:05:58 +0300

    Updates

---
+++ precache-manifest.b6be272a6d4be3abfa0631101c5908fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98f59d0e24e008c1c90c",
+    "url": "/to_watch/static/css/main.29c14886.chunk.css"
+  },
+  {
+    "revision": "98f59d0e24e008c1c90c",
+    "url": "/to_watch/static/js/main.98f59d0e.chunk.js"
+  },
+  {
+    "revision": "1fd33de42fe82c9a1266",
+    "url": "/to_watch/static/js/runtime~main.1fd33de4.js"
+  },
+  {
+    "revision": "0a5fafe0b386eef9436d",
+    "url": "/to_watch/static/css/2.0c004194.chunk.css"
+  },
+  {
+    "revision": "0a5fafe0b386eef9436d",
+    "url": "/to_watch/static/js/2.0a5fafe0.chunk.js"
+  },
+  {
+    "revision": "f298538135327f695c0a33fecec7cc9e",
+    "url": "/to_watch/index.html"
+  }
+];
